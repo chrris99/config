3176152e977009f058a0e23a60845ff1f4c3f3e7
chrris99 <conforti.christian@gmail.com>
2025-02-07 15:27:35 +0100

    feat: configure homebrew

--- flake.nix
+++ flake.nix
@@ -5,22 +5,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.neovim
-        ];
+      environment.systemPackages = [
+        pkgs.neovim
+      ];
+
+      homebrew = {
+        enable = true;
+        casks = [];
+
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -37,7 +44,18 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Christians-MacBook-Pro
     darwinConfigurations."Christians-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "christianconforti";
+            autoMigrate = true;
+          };
+        }
+      ];
     };
   };
 }
