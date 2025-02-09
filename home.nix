e77ec1dae5df7399319eb37ff76de182f25db78c
chrris99 <conforti.christian@gmail.com>
2025-02-09 15:49:54 +0100

    add git config

--- home.nix
+++ home.nix
@@ -6,5 +6,16 @@ _:
 
   home.stateVersion = "24.11";
 
+  programs.git = {
+    enable = true;
+    userName = "chrris99";
+    userEmail = "conforti.christian@gmail.com";
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+    };
+  };
+
   programs.home-manager.enable = true;
 }
