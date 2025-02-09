682141e32933de82d736c98e19395ef4d4fd9ef1
chrris99 <conforti.christian@gmail.com>
2025-02-09 15:42:14 +0100

    chore: import home manager config

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
           home-manager.darwinModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.users.christianconforti = import ./home.nix;
           }
           nix-homebrew.darwinModules.nix-homebrew {
             nix-homebrew = {
