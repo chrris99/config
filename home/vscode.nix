e81c0d8f44b751773fe4c00ff041925c1f712413
chrris99 <conforti.christian@gmail.com>
2025-02-11 19:32:34 +0100

    vscode initial config

--- home/vscode.nix
+++ home/vscode.nix
@@ -7,19 +7,29 @@
     extensions = with pkgs.vscode-extensions; [
       editorconfig.editorconfig
       bbenoist.nix
+      pkief.material-icon-theme
+      zhuangtongfa.material-theme
     ];
 
     userSettings = {
       "workbench.iconTheme" = "material-icon-theme";
+      "workbench.colorTheme" = "One Dark Pro Darker";
       "workbench.startupEditor" = "none";
 
+      # editor
       "editor.tabSize" = 2;
+      "editor.formatOnSave" = true;
       "editor.codeActionsOnSave" = {
         "source.fixAll" = "explicit";
       };
+      "editor.rulers" = [ 120 ];
 
+      # nix
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "nil";
+
+      # typescript
+      "typescript.preferences.importModuleSpecifier" = "relative";
     };
   };
 }
